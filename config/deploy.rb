9268991908a1e21de20e2ac1f7433c27b04cebf1
miao-bpl <rmiao@bpl.org>
2022-10-26 11:44:12 -0400

    change deploy directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,8 @@ set :rails_env, 'production'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/manager/capistrano-demo"
+# set :deploy_to, "/home/manager/capistrano-demo"
+set :deploy_to, "/home/manager/capistrano-staging"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
